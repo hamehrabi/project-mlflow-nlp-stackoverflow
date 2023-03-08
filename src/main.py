dc4f4a43d708cc07f86f60cd48757ccde21f9168
Hamed <mehrabi.hamed@outlook.com>
2023-03-08 10:20:28 +0000

    MLproject and main.py updated

--- src/main.py
+++ src/main.py
@@ -2,13 +2,13 @@ import mlflow
 import argparse
 
 def main():
-    mlflow.set_tracking_uri("http://127.0.0.1:1234")
+    #mlflow.set_tracking_uri("http://127.0.0.1:1234")
 
     with mlflow.start_run() as run:
-        mlflow.run(".", "stage_01", use_conda=False)
-        mlflow.run(".", "stage_02", use_conda=False)
-        mlflow.run(".", "stage_03", use_conda=False)
-        mlflow.run(".", "stage_04", use_conda=False)
+        mlflow.run(".", "stage_01")
+        mlflow.run(".", "stage_02")
+        mlflow.run(".", "stage_03")
+        mlflow.run(".", "stage_04")
 
 if __name__ == '__main__':
     main()
