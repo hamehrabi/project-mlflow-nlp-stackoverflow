1163e0aaf6cbc69c163c75c4be7ac85487d3ebf9
Hamed <mehrabi.hamed@outlook.com>
2023-03-08 08:48:10 +0000

    St-01,  config, common, and conda.yaml updated

--- src/stage_01_get_data.py
+++ src/stage_01_get_data.py
@@ -1,8 +1,9 @@
 import argparse
 import os
 import logging
-from src.utils.common import read_yaml, create_directories
+from src.utils.common import read_yaml, create_directories, copy_files
 import urllib.request as req
+from tqdm import tqdm
 
 STAGE = "stage 01 get data" ## <<< change stage name 
 
@@ -13,25 +14,48 @@ logging.basicConfig(
     filemode="a"
     )
 
+## Use the function below, if you use URL links as your data source
+# def main(config_path):
+#     ## read config files
+#     config = read_yaml(config_path)
+#     source_data_url = config["source_data_url"]
 
-def main(config_path):
-    ## read config files
+#     local_data_dir = config["source_download_dir"]["data_dir"]
+#     create_directories([local_data_dir])
+
+#     data_filename = config["source_download_dir"]["data_file"]
+#     local_data_filepath = os.path.join(local_data_dir, data_filename)
+
+#     logging.info("Download started")
+#     filename, headers = req.urlretrieve(source_data_url, local_data_filepath)
+#     logging.info(f"Download completed")
+#     logging.info(f"Download file is present at: {filename}")
+#     logging.info(f"Download headers: \n{headers}")
+
+
+## Use the function below if you have already downloaded the dataset and the data source is located on your local PC.
+def main(config_path: str) -> None:
+    """get the image data from source to the present working directory
+
+    Args:
+        config_path (str): path to config file
+    """
     config = read_yaml(config_path)
-    source_data_url = config["source_data_url"]
 
-    local_data_dir = config["source_download_dir"]["data_dir"]
-    create_directories([local_data_dir])
+    source_data_dirs = config["source_data_dirs"]
+    local_data_dirs = config["local_data_dirs"]
 
-    data_filename = config["source_download_dir"]["data_file"]
-    local_data_filepath = os.path.join(local_data_dir, data_filename)
+    N = len(source_data_dirs)
+    for source_data_dir, local_data_dir in tqdm(
+        zip(source_data_dirs, local_data_dirs),
+        total=N,
+        colour="red",
+        desc="copying directory:",
+    ):
+        create_directories([local_data_dir])
+        copy_files(source_data_dir, local_data_dir)
 
-    logging.info("Download started")
-    filename, headers = req.urlretrieve(source_data_url, local_data_filepath)
-    logging.info(f"Download completed")
-    logging.info(f"Download file is present at: {filename}")
-    logging.info(f"Download headers: \n{headers}")
-
-
+    logging.info(f"Copied file is present at: {local_data_dirs}")
 
 
 
--- src/utils/common.py
+++ src/utils/common.py
@@ -1,9 +1,11 @@
 import os
 import yaml
 import logging
-import time
 import pandas as pd
 import json
+import shutil
+from tqdm import tqdm
+import time
 
 def read_yaml(path_to_yaml: str) -> dict:
     with open(path_to_yaml) as yaml_file:
@@ -33,3 +35,27 @@ def get_df(path_to_data: str, sep: str="\t") -> pd.DataFrame:
     )
     logging.info(f"The input data frame {path_to_data} size is {df.shape}\n")
     return df
+
+def copy_files(source_data_dir: str, local_data_dir: str) -> None:
+    """Copies file from source to destination directory
+
+    Args:
+        source_data_dir (str): source data directory
+        local_data_dir (str): local data directory
+    """
+    list_of_files = os.listdir(source_data_dir)
+    N = len(list_of_files)
+
+    for filename in tqdm(
+        list_of_files,
+        total=N,
+        desc=f"copying file from {source_data_dir} to {local_data_dir}",
+        colour="green",
+    ):
+        src = os.path.join(source_data_dir, filename)
+        dest = os.path.join(local_data_dir, filename)
+        shutil.copy(src, dest)
+
+    logging.info(
+        f"all the files has been copied from {source_data_dir} to {local_data_dir}"
+    )
